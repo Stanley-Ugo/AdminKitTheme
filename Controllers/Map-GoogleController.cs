e7cda2371222c63a92ad0e9c9dc8d9c82e62ae21
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-03 00:32:16 +0100

    Adding Google-MapController

---
+++ Controllers/Map-GoogleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminKitTheme.Controllers
+{
+    [Route("Map")]
+    public class Map_GoogleController : Controller
+    {
+        [Route("Map")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
