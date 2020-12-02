34853ac53f127710814fd77229bc50f272af3923
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-03 00:01:41 +0100

    Adding InvoiceController

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminKitTheme.Controllers
+{
+    [Route("/invoice")]
+    public class InvoiceController : Controller
+    {
+        [Route("/invoice")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
